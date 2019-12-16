af28768e465265f652fd98d5c74453fbfa0e5bbd
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-16 10:09:09 +0100

    Add print menu

--- theEnigma/src/app/App.java
+++ theEnigma/src/app/App.java
@@ -3,5 +3,15 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
+        boolean isRunning = true;
+
+        while (isRunning) {
+            System.out.println("THE ENIGMA");
+            System.out.println("\n[1]-start the Enigma");
+            System.out.println("[2]-credits");
+            System.out.println("[3]-exit game");
+            System.out.println("\nChoose one option: ");
+            isRunning = false;
+        }
     }
 }
