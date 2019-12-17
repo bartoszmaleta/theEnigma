89467cde35d02e68c101d110091e9710a4202df2
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-17 15:05:11 +0100

    Add decrypting Ceasar in terminal

--- theEnigma/src/enigmaAppInOneClass/Enigma.java
+++ theEnigma/src/enigmaAppInOneClass/Enigma.java
@@ -32,19 +32,37 @@ public class Enigma {
         }
         return result;
     }
-    
+
+    public static StringBuffer decrypt(String text, int g) {
+        g = -g;
+        StringBuffer result = new StringBuffer();
+
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isUpperCase(text.charAt(i))) {
+                char ch = (char) (((int) text.charAt(i) + g - 65) % 26 + 65);
+                result.append(ch);
+            } else {
+                char ch = (char) (((int) text.charAt(i) + g - 97) % 26 + 96);
+                result.append(ch);
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) throws Exception {
 
         System.out.println("Cyphering");
         String text = "ATTACKATONCE";
+        String textToDecrypt = "EXXEGOEXSRGI";
         int s = 4;
-
+        // int g = 4;
 
         int firstElementOfArrayInArgsOfFunction = 0;
         int secondElementOfArrayInArgsOfFunction = 1;
         int thirdElementOfArrayInArgsOfFunction = 2;
         int fourthElementOfArrayInArgsOfFunction = 3;
         int fifthElementOfArrayInArgsOfFunction = 4;
+        int intputedThirdAgument = Integer.parseInt(args[thirdElementOfArrayInArgsOfFunction]);
 
         System.out.println();
         System.out.println("Your first argument of application is: " + args[firstElementOfArrayInArgsOfFunction]);
@@ -70,7 +88,7 @@ public class Enigma {
                 // StringBuffer encrypt(String text, int s)
                 System.out.println();
                 System.out.println("------------------");
-
+                System.out.println("Cyphering");
                 System.out.println("Text   : " + text);
                 System.out.println("Shift  : " + s);
                 System.out.println("Cipher : " + encrypt(text, s));
@@ -85,6 +103,18 @@ public class Enigma {
 
         case "-d":
             System.out.println("You are in decipher mode");
+            switch (args[secondElementOfArrayInArgsOfFunction]) {
+                case "CEASAR":
+                System.out.println();
+                System.out.println("------------------");
+                // stringArgs = args[thirdElementOfArrayInArgsOfFunction];
+
+                System.out.println("Decyphering");
+                System.out.println("Text      : " + textToDecrypt);
+                System.out.println("Shift     : " + g);
+                System.out.println("Decipherd : " + decrypt(textToDecrypt, g));
+
+            }
             break;
         }
 
