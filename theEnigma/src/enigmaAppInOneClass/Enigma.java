87d4192cc898f154962266b19138091083f0fc03
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-17 14:08:37 +0100

    Add ceasar cipher encrypting

--- theEnigma/src/enigmaAppInOneClass/Enigma.java
+++ theEnigma/src/enigmaAppInOneClass/Enigma.java
@@ -18,8 +18,28 @@ import enigmaAppInOneClass.*;
  */
 
 public class Enigma {
+    public static StringBuffer encrypt(String text, int s) {
+        StringBuffer result = new StringBuffer();
+
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isUpperCase(text.charAt(i))) {
+                char ch = (char) (((int) text.charAt(i) + s - 65) % 26 + 65);
+                result.append(ch);
+            } else {
+                char ch = (char) (((int) text.charAt(i) + s - 97) % 26 + 96);
+                result.append(ch);
+            }
+        }
+        return result;
+    }
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+
+        System.out.println("Cyphering");
+        String text = "ATTACKATONCE";
+        int s = 4;
+
+
         int firstElementOfArrayInArgsOfFunction = 0;
         int secondElementOfArrayInArgsOfFunction = 1;
         int thirdElementOfArrayInArgsOfFunction = 2;
@@ -44,8 +64,19 @@ public class Enigma {
             System.out.println("You are in encipher mode");
             switch (args[secondElementOfArrayInArgsOfFunction]) {
             case "CEASAR":
-
                 System.out.println("You are in Ceasar mode");
+                System.out.println();
+
+                // StringBuffer encrypt(String text, int s)
+                System.out.println();
+                System.out.println("------------------");
+
+                System.out.println("Text   : " + text);
+                System.out.println("Shift  : " + s);
+                System.out.println("Cipher : " + encrypt(text, s));
+
+                System.out.println("------------------");
+
                 // CeasarCipher.printbla(); // dont know why it does not compile
                 // enigmaAppInOneClass.CeasarCipher.printbla(); // dont know why it does not
                 // compile
